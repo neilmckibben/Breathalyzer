7721498500323b56eff2ec1a47645ef0cd38dead
neilmckibben <neilmckibben@gmail.com>
2021-03-06 16:29:00 -0500

    update main

--- app/src/main/java/com/example/breathalyzer/MainActivity.java
+++ app/src/main/java/com/example/breathalyzer/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.breathalyzer;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,11 +23,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.breathalyzer.ui.main.SectionsPagerAdapter;
 import com.txusballesteros.widgets.FitChart;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+    private final static int REQUEST_ENABLE_BT = 1;
+    private final static int PERMISSION_CODE = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +57,25 @@ public class MainActivity extends AppCompatActivity {
                 timeLeft.setText("Sober");
             }
         }.start();
+        getBluetoothPermissions();
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(), "Phone does not have bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    // There are paired devices. Get the name and address of each paired device.
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress(); // MAC address
+                    }
+                }
+            }
+        }
 //        ViewPager viewPager = findViewById(R.id.view_pager);
 //        viewPager.setAdapter(sectionsPagerAdapter);
 //        TabLayout tabs = findViewById(R.id.tabs);
@@ -55,5 +89,19 @@ public class MainActivity extends AppCompatActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
+    }
+
+
+    private void getBluetoothPermissions(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(),
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                        PERMISSION_CODE);
+            }
+        }
     }
 }
