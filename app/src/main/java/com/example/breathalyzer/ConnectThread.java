921b16ad263ff530cbae5796500e3592488579ef
Neil Mckibben <neilmckibben@gmail.com>
2021-03-06 19:50:16 -0500

    added bluetooth

---
+++ app/src/main/java/com/example/breathalyzer/ConnectThread.java
@@ -0,0 +1,68 @@
+package com.example.breathalyzer;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.UUID;
+
+public class ConnectThread extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    String TAG = "ConnectThread";
+    public ConnectThread(BluetoothDevice device) {
+        // Use a temporary object that is later assigned to mmSocket
+        // because mmSocket is final.
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+
+        try {
+            UUID uniqueID = UUID.randomUUID(); // -> completely random UUID
+            // Get a BluetoothSocket to connect with the given BluetoothDevice.
+            // MY_UUID is the app's UUID string, also used in the server code.
+            tmp = device.createRfcommSocketToServiceRecord(uniqueID);
+            Log.d(TAG, "connected?");
+        } catch (IOException e) {
+            Log.e(TAG, "Socket's create() method failed", e);
+        }
+        mmSocket = tmp;
+    }
+
+    public void run() {
+        // Cancel discovery because it otherwise slows down the connection.
+        bluetoothAdapter.cancelDiscovery();
+
+        try {
+            // Connect to the remote device through the socket. This call blocks
+            // until it succeeds or throws an exception.
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and return.
+            try {
+                mmSocket.close();
+            } catch (IOException closeException) {
+                Log.e(TAG, "Could not close the client socket", closeException);
+            }
+            return;
+        }
+
+        // The connection attempt succeeded. Perform work associated with
+        // the connection in a separate thread.
+        manageMyConnectedSocket(mmSocket);
+    }
+
+    // Closes the client socket and causes the thread to finish.
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not close the client socket", e);
+        }
+    }
+
+    public void manageMyConnectedSocket(BluetoothSocket socket) {
+        Log.d(TAG, "Actually connected");
+    }}
--- app/src/main/java/com/example/breathalyzer/MainActivity.java
+++ app/src/main/java/com/example/breathalyzer/MainActivity.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.opengl.GLSurfaceView;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -13,12 +14,15 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,6 +37,7 @@ import java.util.Set;
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
     private final static int PERMISSION_CODE = 2;
+    String TAG = "bluetoothStuff";
 
 
     @Override
@@ -43,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
         FitChart fitChart = (FitChart)findViewById(R.id.progress);
         TextView bacValue = findViewById(R.id.bacValue);
         TextView timeLeft = findViewById(R.id.timeLeft);
+        getBluetoothPermissions();
         fitChart.setMinValue(0f);
         fitChart.setMaxValue(1f);
         fitChart.setValue(.56f);
@@ -60,18 +66,28 @@ public class MainActivity extends AppCompatActivity {
         getBluetoothPermissions();
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
+            Log.d(TAG, "No bluetooth");
             Toast.makeText(getApplicationContext(), "Phone does not have bluetooth", Toast.LENGTH_SHORT).show();
         }
         else {
+            Log.d(TAG, "Has bluetooth");
             if (!bluetoothAdapter.isEnabled()) {
                 Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+            }
+            else{
                 Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
                 if (pairedDevices.size() > 0) {
                     // There are paired devices. Get the name and address of each paired device.
                     for (BluetoothDevice device : pairedDevices) {
                         String deviceName = device.getName();
                         String deviceHardwareAddress = device.getAddress(); // MAC address
+                        if (deviceName.equals("Project Zero")) {
+                            Log.d(TAG, "hit");
+                            connect(device);
+                            break;
+                        }
                     }
                 }
             }
@@ -91,17 +107,59 @@ public class MainActivity extends AppCompatActivity {
 //        });
     }
 
+    private void connect(BluetoothDevice device) {
+
+    }
+    private void requestPermission(String[] permission, int code) {
+        ActivityCompat.requestPermissions(
+                MainActivity.this,
+                permission,
+                code);
+        Log.d(TAG, "Done");
+    }
+
 
     private void getBluetoothPermissions(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             if (ContextCompat.checkSelfPermission(getApplicationContext(),
                     Manifest.permission.ACCESS_BACKGROUND_LOCATION)
                     != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(
-                        MainActivity.this,
-                        new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
-                        PERMISSION_CODE);
+                Log.d(TAG, "Needs the stuff");
+                int i = 0;
+                String[] permissions = {Manifest.permission.ACCESS_BACKGROUND_LOCATION, Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION};
+                for (String access : permissions) {
+                    requestPermission(new String[] {access}, i);
+                    i++;
+                }
+//                ActivityCompat.requestPermissions(
+//                        MainActivity.this,
+//                        permissions,
+//                        PERMISSION_CODE);
+                Log.d(TAG, "Done");
+            }
+            else{
+                Log.d(TAG, "Gave permission");
             }
         }
+        else{
+            Log.d(TAG, "sdk not high enough");
+        }
+    }
+
+    // This function is called when user accept or decline the permission.
+// Request Code is used to check which permission called this function.
+// This request code is provided when user is prompt for permission.
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        super
+                .onRequestPermissionsResult(requestCode,
+                        permissions,
+                        grantResults);
+
+        Log.d(TAG, "result");
     }
 }
